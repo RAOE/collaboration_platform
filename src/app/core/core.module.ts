83d6852d81aad345e962c5d31d4ff1f8a72420f1
YuanFengXu <986771570@qq.com>
2021-11-27 12:54:53 +0800

    [feature] add material

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
   imports: [
     CommonModule
